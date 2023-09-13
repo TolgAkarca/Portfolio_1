71b90e76a14eff63ee831eea97319e1cb8c99a94
Mehmet Akarca <takarca@yahoo.com>
2023-09-13 13:32:20 -0400

    ch02lab

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,36 @@
+# Part A 
+import random
 
-#Part A
-weeks = 16
-classes = 5
-tuition = 6000
-cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
+x = random.randrange(7576467576,9999999999999999)
+print(x)
 
 
 #Part B
+
+import turtle  # 1. import modules
+import random
+
+screen = turtle.Screen()
+#create and name turtles 
+tolga = turtle.Turtle()
+aral = turtle.Turtle()
+#color of the turtles
+tolga.color("cyan")
+aral.color("brown")
+#shape of the turtles
+tolga.shape("turtle")
+aral.shape("turtle")
+#starting point of turtles
+tolga.up()  
+aral.up()
+tolga.goto(-100,-20)
+aral.goto(-100,20)
+#Who gonna win
+myRange = (90,200,120,100,50,20,10,70,67,32,87,45,97,148,216)
+aral.forward(random.choice(myRange))
+tolga.forward(random.choice(myRange))
+
+
+screen.exitonclick()
+
+
