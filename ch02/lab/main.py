b1eceed46d14550afb2df1bd51c390ebfbee70c3
Mehmet Akarca <takarca@yahoo.com>
2023-09-14 10:33:45 -0400

    ch02lab

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -24,8 +24,8 @@ aral.shape("turtle")
 #starting point of turtles
 tolga.up()  
 aral.up()
-tolga.goto(-100,-20)
-aral.goto(-100,20)
+tolga.goto(-100,20)
+aral.goto(-100,-20)
 #Who gonna win
 
 myRange = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,40,50)
