8f593d3226203b1d7c5d6fc3057295321e151bb1
Mehmet Akarca <takarca@yahoo.com>
2023-09-14 10:33:23 -0400

    ch02lab

---
+++ ch02/exercises/drawing.py
@@ -0,0 +1,12 @@
+import turtle       
+
+
+userInput = input("Please enter a the number of sides and the length of each side")
+
+sides = (userInput)
+
+screen = turtle.Screen()
+
+
+
+screen.exitonclick()
