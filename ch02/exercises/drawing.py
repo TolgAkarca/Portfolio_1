78f49964b17a81dac4caecb86f4351f6954b041b
Mehmet Akarca <takarca@yahoo.com>
2023-09-18 18:17:44 -0400

    09/18class

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -1,11 +1,17 @@
 import turtle       
 
 
-userInput = input("Please enter a the number of sides and the length of each side")
-
-sides = (userInput)
-
+sides = int(input("Please enter  the number of sides:\n" ))
+sidesLenght = int(input("Please enter lenght of each sides(recommended 75)\n "))
+angle =  360 / sides
 screen = turtle.Screen()
+screen.screensize(1000,1000)
+tolga = turtle.Turtle()
+tolga.color('cyan')
+tolga.shape('turtle')
+for x in range(sides):
+    tolga.forward(sidesLenght)
+    tolga.left(angle)
 
 
 
