e66829438da4776e7fee23b9cb042afacc6fa853
Mehmet Akarca <takarca@yahoo.com>
2023-09-22 12:20:33 -0400

    ch02lab

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,6 +1,7 @@
 # Part A 
 import random
 
+
 x = random.randrange(7576467576,9999999999999999)
 print(x)
 
@@ -37,4 +38,33 @@ for myrange in range(10):
 screen.exitonclick()
 
 
+
+#Part B
+import math 
+import pygame
+pygame.init()
+window = pygame.display.set_mode()
+while True:
+    for event in pygame.event.get():
+        pass
+    window = pygame.display.set_mode()
+    shapes = [3,4,6,20,100,360]
+    for num_sides in shapes :
+
+        side_lenght = 50
+        xpos = 300
+        ypos = 300
+        points = []
+        for i in range(num_sides):
+            angle = 360/num_sides
+            radians = math.radians(angle * i)
+            x = xpos + side_lenght * math.cos(radians)
+            y = ypos + side_lenght * math.sin(radians)
+            points.append([x,y])
+     
+        pygame.draw.polygon(window, 'red', points)
+        pygame.display.flip()
+        window.fill('black')
+        pygame.display.flip
+        pygame.time.wait(300)
 
