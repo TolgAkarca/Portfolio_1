53923523548e204e5bcfd6081744888ade5d2685
Mehmet Akarca <takarca@yahoo.com>
2023-09-29 12:31:48 -0400

    ch03lab

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,26 +1,32 @@
-import turtle #1. import modules
-import random
+  #Part A
 
-#Part A
-window = turtle.Screen() # 2.  Create a screen
-window.bgcolor('lightblue')
+import pygame  
+import random 
+import math
 
-michelangelo = turtle.Turtle() # 3.  Create two turtles
-leonardo = turtle.Turtle()
-michelangelo.color('orange')
-leonardo.color('blue')
-michelangelo.shape('turtle')
-leonardo.shape('turtle')
+pygame.init()
+screen = pygame.display.set_mode((500,500))
+window = screen.get_rect()
 
-michelangelo.up() # 4. Pick up the pen so we don’t get lines
-leonardo.up()
-michelangelo.goto(-100,20)
-leonardo.goto(-100,-20)
+while True:
+    for event in pygame.event.get():
+        pass
+    screen.fill('blue')
+    pygame.draw.circle(screen , 'white', (250,250) , 250)
+    pygame.draw.line(screen , 'black', (250,0) ,(250,500) , 5 )
+    pygame.draw.line(screen , 'black', (0,250) ,(500,250) , 5 )
+  
+    #Part B
+    for x in range(10) :
+      x = random.randrange(0,500)
+      y = random.randrange(0,500)
+      distance_from_center = math.hypot(250-x,250-y) 
+      is_in_circle = distance_from_center <= 500/2 
+      if is_in_circle  :
+         pygame.draw.circle(screen , 'green' , (x ,y) , 10)
+      else: pygame.draw.circle(screen , 'red',(x ,y) , 10)
+     
+      pygame.display.flip()
+      pygame.time.wait(500)
+    
 
-## 5. Your PART A code goes here
-
-
-# PART B - complete part B here
-
-
-window.exitonclick()
